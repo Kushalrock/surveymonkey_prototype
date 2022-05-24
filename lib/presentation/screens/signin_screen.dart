4293e69121b685c0bec056187e4029a1d876f5e9
Kushal Agrawal <agrawalskushal@gmail.com>
2022-05-24 23:43:25 +0530

    The overlaypopup

--- lib/presentation/screens/signin_screen.dart
+++ lib/presentation/screens/signin_screen.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:surveymonkey_prototype/logic/cubit/auth_cubit.dart';
@@ -17,12 +16,141 @@ class _SignInScreenState extends State<SignInScreen> {
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  int cardIndex = 0;
+  int lastIndex = 2;
+  OverlayEntry? overlayEntry;
 
   void _authenticateWithEmailAndPassword(context) {
     if (_formKey.currentState!.validate()) {
       BlocProvider.of<AuthCubit>(context)
           .signInRequested(_emailController.text, _passwordController.text);
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addPostFrameCallback((_) => showOverlay());
+  }
+
+  List<Image> cards = [
+    Image.asset(
+      "assets/img_five.png",
+      width: 200,
+      height: 200,
+    ),
+    Image.asset(
+      "assets/img_five.png",
+      width: 200,
+      height: 200,
+    ),
+    Image.asset(
+      "assets/img_five.png",
+      width: 200,
+      height: 200,
+    ),
+  ];
+  List<String> textFirstLine = [
+    "Only 5 questions a day, and receive loads of points",
+    "guyij",
+    "guyij",
+  ];
+  List<String> textSecondLine = [
+    "Redeem them for real money",
+    "guyij",
+    "guyij",
+  ];
+
+  Widget GetCardWidget() {
+    return Positioned(
+      child: Card(
+        elevation: 20,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: Column(
+          children: [
+            cards[cardIndex],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+              child: Text(textFirstLine[cardIndex]),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+              child: Text(textSecondLine[cardIndex]),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Row(
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        if (cardIndex > 0) {
+                          setState(() {
+                            cardIndex -= 1;
+                            hideOverlay();
+                            overlayEntry = OverlayEntry(
+                              builder: (context) {
+                                return GetCardWidget();
+                              },
+                            );
+                            Overlay.of(context)?.insert(overlayEntry!);
+                          });
+                        }
+                      });
+                    },
+                    icon: Icon(Icons.arrow_left),
+                    label: Text(""),
+                  ),
+                  SizedBox(
+                    width: 200,
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      if (cardIndex >= lastIndex) {
+                        hideOverlay();
+                        print("Yes");
+                      } else {
+                        setState(() {
+                          cardIndex += 1;
+                        });
+                        print(cardIndex);
+                        hideOverlay();
+                        overlayEntry = OverlayEntry(
+                          builder: (context) {
+                            return GetCardWidget();
+                          },
+                        );
+                        Overlay.of(context)?.insert(overlayEntry!);
+                      }
+                    },
+                    icon: Icon(Icons.arrow_right),
+                    label: Text(""),
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              ),
+            )
+          ],
+        ),
+      ),
+      left: MediaQuery.of(context).size.width * 0.08,
+      top: MediaQuery.of(context).size.height * 0.3,
+    );
+  }
+
+  void showOverlay() {
+    overlayEntry = OverlayEntry(
+      builder: (context) {
+        return GetCardWidget();
+      },
+    );
+    final overlay = Overlay.of(context);
+    overlay?.insert(overlayEntry!);
+  }
+
+  void hideOverlay() {
+    overlayEntry?.remove();
+    overlayEntry = null;
   }
 
   @override
