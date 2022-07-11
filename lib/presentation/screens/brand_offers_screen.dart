301ec358e0356473b80240ac7995b0b78716f03a
Kushal Agrawal <agrawalskushal@gmail.com>
2022-07-11 18:17:41 +0530

    Brand Offers Screen Complete

--- lib/presentation/screens/brand_offers_screen.dart
+++ lib/presentation/screens/brand_offers_screen.dart
@@ -6,7 +6,22 @@ class BrandOffersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
+      appBar: AppBar(
+        title: const Text('CoinKick Offers'),
+        elevation: 0,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 0.0),
+              colors: <Color>[
+                Color.fromARGB(255, 9, 32, 63),
+                Color.fromARGB(255, 83, 120, 149)
+              ],
+            ),
+          ),
+        ),
+      ),
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
@@ -22,22 +37,55 @@ class BrandOffersScreen extends StatelessWidget {
         child: Center(
           child: SingleChildScrollView(
             child: Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: MediaQuery.of(context).size.height * 0.8,
+              width: MediaQuery.of(context).size.width * 0.75,
+              height: MediaQuery.of(context).size.height * 0.75,
               decoration: const BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(20)),
                   color: Colors.white),
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Text(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text(
                       "Coinkick Offers",
                       style: TextStyle(
                         fontSize: 30,
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
+                      ),
+                    ),
+                    Image.asset(
+                      "assets/sign_in_screen_3.png",
+                      width: 200,
+                      height: 200,
+                    ),
+                    Column(
+                      children: const [
+                        Text(
+                          "No Available Offers",
+                          style: TextStyle(
+                            fontSize: 19,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          "Check back later!",
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Go to Main Dashboard"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromARGB(255, 0, 145, 255),
                       ),
                     ),
                   ],
